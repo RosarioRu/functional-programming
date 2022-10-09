896dd231b497f3d4d98614eab082603a4a6c9255
RosarioRu <mariarruval@gmail.com>
2022-10-08 23:57:10 -0700

    WIP: Add pseudocode for string compression solution.

---
+++ StringCompression.js
@@ -0,0 +1,25 @@
+/*
+Question #3: Compressing Strings
+Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
+
+Example
+Input: "aaabccdddda"
+
+Output: "3ab2c4da"
+*/
+
+/* Plan and steps:
+      1. Create an empty array to store and tally characters
+      2. Loop through the string's characters
+            if character doesn't yet exist in empty array, push it into the array but also push a 1 as a counter (two dimensional array?)
+            if character DOES exist, increment the counter.
+      3. Should end up with array holding the count and character [[3][a], [1][b]].
+      4. make array a string again by looping through 2-dim array and using if/else statements to only return element[0] if it is greater than 1. 
+            if element[0]>1, 'return' this plus element[1]
+            maybe concat these to an empty string made at the beginning of this loop, then return said string at end.
+*/
+
+
+      
+
+      
