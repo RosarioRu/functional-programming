a918b0f4c0bb67382c026c41dd1bd49509d74c7e
RosarioRu <mariarruval@gmail.com>
2022-10-08 22:35:31 -0700

    Add recursive solution - had to include a global variable.

--- ArrayDeduping.js
+++ ArrayDeduping.js
@@ -26,25 +26,32 @@ function unduplicating(array) {
   return editedArray;
 };
 
-// recursive solution:
-
+// recursive solution: not sure how to do this without a global variable!
+let editedArray = [];
 function unduplicatingRecursive(array) {
-  let editedArray = [];
+  
   if((!Array.isArray(array)) || (array.length === 0)) {
     return "Please enter a valid array.";
   }
   if (array.length === 1) {
     if(editedArray.includes(array[0])) {
-      return;
+      return editedArray;
     } else {
       editedArray.push(array[0]);
       return editedArray;
     }
   } else {
   //recursion below:
-  if (!editedArray.includes(array[0]) {
-    editedArray.push(array[0]);
-    return unduplicatingRecursive(array.slice(1));
+    if (!editedArray.includes(array[0])) {
+      editedArray.push(array[0]);
+      return unduplicatingRecursive(array.slice(1));
+    } else {
+      return unduplicatingRecursive(array.slice(1));
+    }
   }
+};
+
+
+
   
 
