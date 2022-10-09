6a622e84d2295f33223877b683e5b9967fa21fca
RosarioRu <mariarruval@gmail.com>
2022-10-08 20:05:01 -0700

    WIP: attemping a for loop to solve problem.

---
+++ StringToUrl.js
@@ -0,0 +1,21 @@
+/*Question #1: Turning Strings to URLs
+URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
+
+You may not use the replace() method or regular expressions to solve this problem. Solve the problem with and without recursion.
+
+Example
+Input: "Jasmine Ann Jones"
+
+Output: "Jasmine%20Ann%20Jones"
+*/
+
+function stringToUrl(string) {
+  
+  for(i = 0; i<names.length; i++) {
+    if (names[i] === " ") {
+      newString.concat("%20");
+    } else {
+      newString.concat(names[i]);
+    }
+    return newString;
+  };
